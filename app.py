4be593b3b3c50c9cc681dd56ef01d2ee6cbca037
jmgrd98 <jmgrd98@gmail.com>
2025-01-22 16:28:28 -0300

    allow prod url

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"],
+    allow_origins=['https://aida-data.vercel.app/'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
