4949c7eae020f949282fbfc327a5d85f69a5813e
jmgrd98 <jmgrd98@gmail.com>
2025-01-22 16:32:12 -0300

    commit

--- app.py
+++ app.py
@@ -17,8 +17,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
-    allow_origins=['https://aida-data.vercel.app/'],
+    allow_origins=[
+        "http://localhost:3000",
+        'https://aida-data.vercel.app/'
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
